284f6464683d5c8619b9a681e0764d9e46f37832
Ian <ianb96@gmail.com>
2017-03-18 21:51:06 -0400

    setup stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxHealth;
+	public float curHealth;
+
+	public float moveSpeed;
+
+	public CharacterController cc;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float vx = Input.GetAxis("Horizontal");
+		float vz = Input.GetAxis("Vertical");
+
+		
+	}
+}
