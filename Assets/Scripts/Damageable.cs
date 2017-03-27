e4a1886cf977e8cad696940290caf0a67cd3a90b
Ian <ianb96@gmail.com>
2017-03-27 04:56:32 -0400

    starting main scripts

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour {
+
+    public float maxHealth;
+    public float curHealth;
+	
+    public void TakeDamage(float amount) {
+        curHealth-=amount;
+        if (curHealth<=0) {
+            Die();
+        }
+    }
+    public void Die() {
+        // nothing
+    }
+
+}
