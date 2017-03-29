e073389ba7f1dc9a1fb37b6fc25e289b7c4c6352
ianb96 <ianb96@gmail.com>
2017-03-29 13:11:32 -0400

    run fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : Damageable
     public bool canMove = true;
     public float maxSpeed = 4;
     public float accelRate = 0.5f;
+    bool grounded = false;
     public int numJumps = 2;
     int curJumps;
     public float jumpHeight = 5;
@@ -15,8 +16,9 @@ public class Player : Damageable
     float jumpDur = 2;
     float jumpSpeed = 2;
     float grav = 10;
-    bool inAttack = false;
-    public bool grounded = false;
+
+    bool attacking = false;
+    bool inAttackSwing = false;
     public SpriteRenderer psprite;
     Rigidbody2D rb;
     Animator anim;
@@ -32,6 +34,12 @@ public class Player : Damageable
     /// any of the Update methods is called the first time.
     void Start()
     {
+        RecalculateJumpArc();
+    }
+
+    /// Uses jumpDist, jumpHeight, and maxSpeed to determine jumpDur, jumpSpeed, and grav
+    public void RecalculateJumpArc()
+    {
         jumpDur = 0.5f * jumpDist / maxSpeed;
         jumpSpeed = 2 * jumpHeight / jumpDur;
         grav = -jumpSpeed / jumpDur;
@@ -44,25 +52,30 @@ public class Player : Damageable
         {
             Move();
         }
-        if (!inAttack)
+        if (Input.GetButtonDown("Attack"))
         {
-            if (Input.GetButtonDown("Attack"))
-            {
-                StartCoroutine(Attack());
-            }
+            attacking = true;
+            anim.SetBool("Attacking", true);
+            // StartCoroutine(Attack());
+        }
+        if (!inAttackSwing && Input.GetButtonUp("Attack"))
+        {
+            attacking = false;
+            anim.SetBool("Attacking", false);
         }
     }
 
-    // Get input and move the player with rigidbody
+    /// Get input and move the player with rigidbody
     void Move()
     {
         // left / right
         float hor = Input.GetAxis("Horizontal");
         // float ver = Input.GetAxis("Vertical");
-        if (Mathf.Abs(hor) > 0.05f) {
+        if (Mathf.Abs(hor) > 0.05f)
+        {
             if (rb.velocity.x <= maxSpeed && rb.velocity.x >= -maxSpeed)
             {
-                rb.velocity += Vector2.right * hor * accelRate;
+                rb.velocity += Vector2.right * hor * accelRate * (attacking ? 0.2f:1f);
             }
             else if (rb.velocity.x > maxSpeed)
             {
@@ -72,37 +85,74 @@ public class Player : Damageable
             {
                 rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
             }
-        } else {
-            rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(0,rb.velocity.y), 100*accelRate*Time.deltaTime);
         }
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-        if (rb.velocity.x < 0) {
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(0, rb.velocity.y), 100 * accelRate * Time.deltaTime);
+        }
+        // sprite facing
+        if (hor < 0)
+        {
             psprite.flipX = true;
-        } else if (rb.velocity.x > 0) {
+        }
+        else if (hor > 0 || rb.velocity.x > 0.01)
+        {
             psprite.flipX = false;
         }
+        else if (rb.velocity.x < 0) // its like this to fix edge cases
+        {
+            psprite.flipX = true;
+        }
+        float speed = Mathf.Abs(rb.velocity.x);
+        anim.SetFloat("Speed", speed > 0.05 ? speed : 0);
 
         // falling 
-        if (grounded) {
+        if (grounded)
+        {
             anim.SetBool("Jumping", false);
             anim.SetBool("Falling", false);
             curJumps = 0;
-            rb.AddForce(-0.05f*Vector2.up, ForceMode2D.Force);
-        }else {
-            rb.AddForce(grav*Vector2.up, ForceMode2D.Force);
-            if (rb.velocity.y < 0) {
+            rb.AddForce(-0.05f * Vector2.up, ForceMode2D.Force);
+        }
+        else
+        {
+            rb.AddForce(grav * Vector2.up, ForceMode2D.Force);
+            if (rb.velocity.y < 0)
+            {
                 anim.SetBool("Falling", true);
             }
         }
+
         // jumping
         if (curJumps < numJumps && Input.GetButtonDown("Jump"))
         {
-            // rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             anim.SetBool("Jumping", true);
             curJumps++;
         }
     }
+
+    // start the attack sequence
+    // IEnumerator Attack()
+    // {
+    //     // while
+    //     yield return null;
+
+    // }
+
+    /// Anim will call this to indicate the point of no return for the attack swing
+    public void AttackSwingStart()
+    {
+        inAttackSwing = true;
+    }
+    /// Anim will call this to indicate the attack swing is over
+    public void AttackSwingEnd() {
+        inAttackSwing = false;
+        attacking = false;
+        anim.SetBool("Attacking", false);
+        //TODO: combo attacks
+    }
+
     /// <summary>
     /// Sent when an incoming collider makes contact with this object's
     /// collider (2D physics only).
@@ -110,21 +160,25 @@ public class Player : Damageable
     /// <param name="other">The Collision2D data associated with this collision.</param>
     void OnCollisionStay2D(Collision2D other)
     {
-        float dot = Vector3.Dot(other.contacts[0].normal, Vector3.up);
-        // Debug.Log(dot);
-        grounded = false;
-        if (dot > 0.75f)
-        { 
-            // hit the ground
-            grounded = true;
-        }
-        else if (dot > 0.25f)
+        //grounded = false;
+        foreach (ContactPoint2D contact in other.contacts)
         {
-            // hit a wall
-            Debug.Log(other.relativeVelocity);
-            rb.AddForce(2*other.relativeVelocity, ForceMode2D.Impulse);
+            float dot = Vector3.Dot(contact.normal, Vector3.up);
+            // Debug.Log(dot);
+            if (dot > 0.75f)
+            {
+                // hit the ground
+                grounded = true;
+            }
+            else if (dot > -0.25f && dot < 0.25f)
+            {
+                // hit a wall; prevent clinging
+                Vector2 depenetrationForce = -1 * other.relativeVelocity.x * Vector2.right + Vector2.up * grav * Time.deltaTime;
+                rb.AddForce(depenetrationForce, ForceMode2D.Impulse);
+            }
         }
     }
+
     /// <summary>
     /// Sent when a collider on another object stops touching this
     /// object's collider (2D physics only).
@@ -133,10 +187,5 @@ public class Player : Damageable
     void OnCollisionExit2D(Collision2D other)
     {
         grounded = false;
-    }
-    // start the attack sequence
-    IEnumerator Attack()
-    {
-        yield return null;
     }
 }
